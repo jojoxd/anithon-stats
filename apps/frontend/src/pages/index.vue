90c8623c4a6a87a6a2ed5c0ab1b9bfdac6e0f2d2
Joey Vos <contact@jojoxd.nl>
2022-12-11 22:12:19 +0100

    Add UserLists to index when logged in

--- apps/frontend/src/pages/index.vue
+++ apps/frontend/src/pages/index.vue
@@ -1,52 +1,28 @@
 <script lang="ts">
-  import {defineComponent, ref, watch} from "vue";
-  import {useTitle} from "../composition/useTitle";
-  import {debouncedRef} from "@vueuse/core";
+  import {defineComponent} from "vue";
+  import {useCurrentUser} from "../composition/useCurrentUser";
 
   export default defineComponent({
     setup()
     {
-			const user = ref(null);
-      const debouncedUser = debouncedRef<string>(user, 750);
+      const {
+        currentUser,
+      } = useCurrentUser();
 
-      const title = useTitle();
-
-      watch(user, () => {
-        if(user.value?.length === 0) {
-          title.value = null;
-          return;
-        }
-
-        title.value = `Search ${user.value}`;
-      });
-
-      return { user, debouncedUser };
+      return {
+        currentUser,
+      };
     }
   });
 </script>
 
 <template>
-	<v-container fluid>
-		<v-row>
-			<v-col class="d-flex justify-center">
-				<v-card max-width="600" min-width="300" class="flex-fill">
-					<v-card-title>
-						Your Username
-					</v-card-title>
-
-					<v-text-field v-model="user" label="Username" />
-				</v-card>
-			</v-col>
-		</v-row>
-
-		<v-row>
-			<v-col>
-				<div v-if="debouncedUser">
-					<UserLists :user="debouncedUser" />
-				</div>
-			</v-col>
-		</v-row>
-	</v-container>
+  <div v-if="currentUser?.isAuthenticated">
+    <UserLists :user-name="currentUser.name" />
+  </div>
+  <div v-else>
+    It's pretty empty here...
+  </div>
 </template>
 
 <style scoped lang="scss">
